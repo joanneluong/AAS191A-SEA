0269ba32164ff46160128c48489e04238ae88db5
Joanne Luong <joanneluong@PersonalMicrosoftSoftware.ucla.edu>
2023-06-12 15:20:19 -0700

    collapsible container for locations

--- js/init.js
+++ js/init.js
@@ -42,23 +42,24 @@ function addMarker(data){
         circleOptions.fillColor = "blue"
         L.circleMarker([data.lat,data.lng], circleOptions).addTo(map).bindPopup(`<h2>${data["Where at UCLA do you feel most supported as a Southeast Asian student?"]}</h2>
         <h3>${data["Why do you feel supported in that location? "]}</h3>`)
-        createButtons(data.lat,data.lng, data["Where at UCLA do you feel most supported as a Southeast Asian student?"])
+        createDropdown(data.lat,data.lng, data["Where at UCLA do you feel most supported as a Southeast Asian student?"])
     }
     else if(data["Do you feel that UCLA is equipped with adequate resources resources/programs/events/ etc. to support Southeast Asian students? (either student led or university led)"] == "No"){
         circleOptions.fillColor = "red"
         L.circleMarker([data.lat,data.lng], circleOptions).addTo(map).bindPopup(`<h2>${data["Where at UCLA do you feel most supported as a Southeast Asian student?"]}</h2>
         <h3>${data["Why do you feel supported in that location? "]}</h3>`)
-        createButtons(data.lat,data.lng, data["Where at UCLA do you feel most supported as a Southeast Asian student?"])
+        createDropdown(data.lat,data.lng, data["Where at UCLA do you feel most supported as a Southeast Asian student?"])
     }
     else{
         circleOptions.fillColor = "green"
         L.circleMarker([data.lat,data.lng], circleOptions).addTo(map).bindPopup(`<h2>${data["Where at UCLA do you feel most supported as a Southeast Asian student?"]}</h2>
         <h3>${data["Why do you feel supported in that location? "]}</h3>`)
-        createButtons(data.lat,data.lng, data["Where at UCLA do you feel most supported as a Southeast Asian student?"])
+        createDropdown(data.lat,data.lng, data["Where at UCLA do you feel most supported as a Southeast Asian student?"])
     }
 }
 
-function createButtons(lat,lng, loc){
+function createDropdown(lat,lng, loc){
+    // old button functions
     const newButton = document.createElement("button"); // adds a new button
     newButton.id = "button"; // gives the button a unique id
     newButton.innerHTML = loc; // gives the button a title
@@ -68,16 +69,31 @@ function createButtons(lat,lng, loc){
         const zoom = 17;
         map.flyTo([lat,lng],zoom); //this is the flyTo from Leaflet
     })
-    const spaceForButtons = document.getElementById('placeForButtons')
+    // const spaceForButtons = document.getElementById('placeForButtons')
+    // spaceForButtons.appendChild(newButton);//this adds the button to our page.
+
+    // const option = document.createElement("option")
+    // option.text = loc;
+    // option.addEventListener('click', function(){
+    //     const zoom = 17;
+    //     map.flyTo([lat,lng],zoom); //this is the flyTo from Leaflet
+    // })
+    // const dropdownMenu = document.getElementById('myDropdown')
+    // dropdownMenu.appendChild(option)
+
+    const spaceForButtons = document.getElementById('locations')
     spaceForButtons.appendChild(newButton);//this adds the button to our page.
+
 }
 
-// // function addResponses(data){
-//     const newResponse = 
-//     const spaceForResponses = document.getElementById('responses')
-//     spaceForResponses.appendChild(newResponse);
-// //     <h2>${data["Where at UCLA do you feel most supported as a Southeast Asian student?"]}</h2>
-// // }
+function addResponse(data){
+    const container = document.createElement("container");
+    container.id = "response";
+    container.textContent = "sldkf";
+    const spaceForResponse = document.getElementById('responses')
+    spaceForResponse.appendChild(container);
+}
+
 
 const dataURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5fFbVnixndmfVEIDIrd-u4b-slbVGsNFv2OmRYxS3fTLNFFEmXdXDF4kuLuuItvaC8Pu_rd8In5TE/pub?output=csv"
 
@@ -92,6 +108,7 @@ function loadData(url){
 function processData(results){
     results.data.forEach(data => {
         addMarker(data)
+        addResponse(data)
         // addResponse(data)
     })
     supported.addTo(map)
@@ -104,3 +121,38 @@ function processData(results){
 
 loadData(dataURL)
 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+  
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
